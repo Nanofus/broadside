e5a4f6e0bd572c274a3e115ce2643826ba85690b
Ville Talonpoika <talonvi@gmail.com>
2017-06-01 00:54:50 +0300

    Refactoring

--- app/routes.js
+++ app/routes.js
@@ -3,101 +3,41 @@
 var express = require('express');
 var path = require('path');
 
-var finalhandler = require('finalhandler')
-var serveStatic = require('serve-static')
-
-var serve = serveStatic('public', {'index': ['index.html']})
-
 module.exports = function(app, passport) {
 
 	// =====================================
 	// API =================================
 	// =====================================
 
-	var router = express.Router();              // get an instance of the express Router
-	// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+	var router = express.Router();
 	router.get('/', isLoggedIn, function(req, res) {
-	    res.json({ message: 'hooray! welcome to our api!' });
+	  res.json({ message: 'hooray! welcome to our api!' });
 	});
+	router.post('/login', passport.authenticate('local-login'), function(req, res) {
+    if (req.body.remember) {
+      req.session.cookie.maxAge = 1000 * 60 * 3;
+    } else {
+      req.session.cookie.expires = false;
+    }
+    res.json(req.user);
+  });
+	router.post('/signup', passport.authenticate('local-signup'), function(req, res) {
+		res.json(req.user);
+	});
+	router.get('/logout', function(req, res) {
+		res.json({ message: "success" });
+	});
+	router.get('/user-data', function(req, res) {
+		res.json(req.user);
+	});
+	
 	app.use('/api', router);
 
-	// =====================================
-	// HOME PAGE (with login links) ========
-	// =====================================
-	app.get('/', function(req, res) {
-		// render the page and pass in any flash data if it exists
-		serve(req, res, finalhandler(req, res))
-	});
-
-	// =====================================
-	// LOGIN ===============================
-	// =====================================
-	// show the login form
-	app.get('/login', function(req, res) {
-		// render the page and pass in any flash data if it exists
-		res.render('login.ejs', { message: req.flash('loginMessage') });
-	});
-
-	// process the login form
-	app.post('/login', passport.authenticate('local-login', {
-            successRedirect : '/profile', // redirect to the secure profile section
-            failureRedirect : '/login', // redirect back to the signup page if there is an error
-            failureFlash : true // allow flash messages
-		}),
-        function(req, res) {
-            console.log("hello");
-
-            if (req.body.remember) {
-              req.session.cookie.maxAge = 1000 * 60 * 3;
-            } else {
-              req.session.cookie.expires = false;
-            }
-        res.send(req.user);
-    });
-
-	// =====================================
-	// SIGNUP ==============================
-	// =====================================
-	// show the signup form
-	app.get('/signup', function(req, res) {
-		// render the page and pass in any flash data if it exists
-		res.render('signup.ejs', { message: req.flash('signupMessage') });
-	});
-
-	// process the signup form
-	app.post('/signup', passport.authenticate('local-signup', {
-		successRedirect : '/profile', // redirect to the secure profile section
-		failureRedirect : '/signup', // redirect back to the signup page if there is an error
-		failureFlash : true // allow flash messages
-	}));
-
-	// =====================================
-	// PROFILE SECTION =========================
-	// =====================================
-	// we will want this protected so you have to be logged in to visit
-	// we will use route middleware to verify this (the isLoggedIn function)
-	app.get('/profile', isLoggedIn, function(req, res) {
-		res.render('profile.ejs', {
-			user : req.user // get the user out of session and pass to template
-		});
-	});
-
-	// =====================================
-	// LOGOUT ==============================
-	// =====================================
-	app.get('/logout', function(req, res) {
-		req.logout();
-		res.redirect('/');
-	});
 };
 
 // route middleware to make sure
 function isLoggedIn(req, res, next) {
-
-	// if user is authenticated in the session, carry on
 	if (req.isAuthenticated())
 		return next();
-
-	// if they aren't redirect them to the home page
-	res.redirect('/');
+	res.json({ message: "You are not logged in." });
 }
